acaf62b4216edf65ae75017ebda6d6a5662b8201
С - 3 < - 3@DESKTOP-E0FTS21>
2023-07-12 22:03:30 +0300

    Добавьте файлы проекта.

---
+++ RichTextLab/Form1.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace RichTextLab
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.rtbDoc = new System.Windows.Forms.RichTextBox();
+            this.btnBold = new System.Windows.Forms.Button();
+            this.btnItalic = new System.Windows.Forms.Button();
+            this.btnUsal = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnLoad = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // rtbDoc
+            // 
+            this.rtbDoc.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.rtbDoc.Location = new System.Drawing.Point(21, 23);
+            this.rtbDoc.Name = "rtbDoc";
+            this.rtbDoc.Size = new System.Drawing.Size(528, 192);
+            this.rtbDoc.TabIndex = 0;
+            this.rtbDoc.Text = "";
+            // 
+            // btnBold
+            // 
+            this.btnBold.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnBold.Location = new System.Drawing.Point(584, 32);
+            this.btnBold.Name = "btnBold";
+            this.btnBold.Size = new System.Drawing.Size(161, 47);
+            this.btnBold.TabIndex = 1;
+            this.btnBold.Text = "Жирный";
+            this.btnBold.UseVisualStyleBackColor = true;
+            this.btnBold.MouseClick += new System.Windows.Forms.MouseEventHandler(this.btnBold_MouseClick);
+            // 
+            // btnItalic
+            // 
+            this.btnItalic.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnItalic.Location = new System.Drawing.Point(584, 85);
+            this.btnItalic.Name = "btnItalic";
+            this.btnItalic.Size = new System.Drawing.Size(161, 49);
+            this.btnItalic.TabIndex = 1;
+            this.btnItalic.Text = "Курсив";
+            this.btnItalic.UseVisualStyleBackColor = true;
+            this.btnItalic.MouseClick += new System.Windows.Forms.MouseEventHandler(this.btnBold_MouseClick);
+            // 
+            // btnUsal
+            // 
+            this.btnUsal.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnUsal.Location = new System.Drawing.Point(584, 140);
+            this.btnUsal.Name = "btnUsal";
+            this.btnUsal.Size = new System.Drawing.Size(161, 47);
+            this.btnUsal.TabIndex = 1;
+            this.btnUsal.Text = "Обычный";
+            this.btnUsal.UseVisualStyleBackColor = true;
+            this.btnUsal.MouseClick += new System.Windows.Forms.MouseEventHandler(this.btnBold_MouseClick);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnSave.Location = new System.Drawing.Point(584, 193);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(161, 47);
+            this.btnSave.TabIndex = 1;
+            this.btnSave.Text = "Сохранить";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.MouseClick += new System.Windows.Forms.MouseEventHandler(this.btnSave_MouseClick);
+            // 
+            // btnLoad
+            // 
+            this.btnLoad.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnLoad.Location = new System.Drawing.Point(584, 246);
+            this.btnLoad.Name = "btnLoad";
+            this.btnLoad.Size = new System.Drawing.Size(161, 47);
+            this.btnLoad.TabIndex = 1;
+            this.btnLoad.Text = "Открыть";
+            this.btnLoad.UseVisualStyleBackColor = true;
+            this.btnLoad.MouseClick += new System.Windows.Forms.MouseEventHandler(this.btnBold_MouseClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnLoad);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnUsal);
+            this.Controls.Add(this.btnItalic);
+            this.Controls.Add(this.btnBold);
+            this.Controls.Add(this.rtbDoc);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseClick);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox rtbDoc;
+        private System.Windows.Forms.Button btnBold;
+        private System.Windows.Forms.Button btnItalic;
+        private System.Windows.Forms.Button btnUsal;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnLoad;
+    }
+}
+
---
+++ RichTextLab/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace RichTextLab
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnBold_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (rtbDoc.SelectedText != null)
+            {
+                if (((Button)sender).Text == "Жирный")
+                {
+                   
+                    rtbDoc.SelectionFont = new Font("Microsoft Sans Serif", 12, FontStyle.Bold);
+                }
+                else
+                {
+                    if (((Button)sender).Text == "Курсив")
+                    {
+                        rtbDoc.SelectionFont = new Font("Microsoft Sans Serif", 12, FontStyle.Italic);
+                    }
+                    else
+                    {
+                        rtbDoc.SelectionFont = new Font("Microsoft Sans Serif", 12, FontStyle.Regular);
+                    }
+                }
+                
+            }
+        }
+
+        private void btnSave_MouseClick(object sender, MouseEventArgs e)
+        {
+            rtbDoc.SaveFile("Dock.rtf");
+        }
+
+        private void Form1_MouseClick(object sender, MouseEventArgs e)
+        {
+
+        }
+    }
+}
